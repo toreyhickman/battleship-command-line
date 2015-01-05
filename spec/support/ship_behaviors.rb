f114c358f38bcbdabf7611194c02b051df933b2e
Torey Hickman <torey@toreyhickman.com>
2015-01-05 16:08:55 -0600

    Ungroup example groups with just one example

--- spec/support/ship_behaviors.rb
+++ spec/support/ship_behaviors.rb
@@ -1,35 +1,27 @@
 shared_examples "a battleship ship" do |desired_size|
   let(:ship) { described_class.new }
 
-  describe "ship size" do
-    it "has a size of #{desired_size}" do
-      expect(ship.size).to eq desired_size
-    end
+  it "has a size of #{desired_size}" do
+    expect(ship.size).to eq desired_size
   end
 
-  describe "times ship has been hit" do
-    it "keeps track of hits" do
-      expect(ship.hits).to eq 0
-    end
+  it "keeps track of hits" do
+    expect(ship.hits).to eq 0
   end
 
-  describe "#increase_hit_count" do
-    it "can be hit" do
-      expect {ship.increase_hit_count}.to change {ship.hits}.by(1)
-    end
+  it "can be hit" do
+    expect {ship.increase_hit_count}.to change {ship.hits}.by(1)
   end
 
-  describe "#sunk?" do
-    it "can be unsunk" do
-      expect(ship.sunk?).to be false
+  it "can be unsunk" do
+    expect(ship.sunk?).to be false
+  end
+
+  it "can be sunk" do
+    until ship.hits >= ship.size
+      ship.increase_hit_count
     end
 
-    it "can be sunk" do
-      until ship.hits >= ship.size
-        ship.increase_hit_count
-      end
-
-      expect(ship.sunk?).to be true
-    end
+    expect(ship.sunk?).to be true
   end
 end
