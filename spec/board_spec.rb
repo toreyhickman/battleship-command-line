80445ad50b498c08306940c448ddf5e367ce5dc8
Torey Hickman <torey@toreyhickman.com>
2015-09-25 09:01:18 -0500

    Stop testing for class of a board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,10 +4,6 @@ describe Board do
   let(:created_board) { Board.create }
 
   describe ".create" do
-    it "makes a board" do
-      expect(created_board).to be_an_instance_of Board
-    end
-
     it "sets up the board with 100 spaces" do
       expect(created_board.spaces.size).to eq 100
     end
